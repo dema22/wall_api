f63b4e49c34ce19b18f54d97fb7eeb835e880b55
dema22 <felipedemaria@hotmail.com>
2022-05-12 20:36:33 -0300

    Add time stamp to the Post Model. Also now we can create a post

--- thewallapi/models.py
+++ thewallapi/models.py
@@ -2,6 +2,6 @@ from django.db import models
 
 # Create your models here.
 class Post (models.Model):
-    created_at = models.DateTimeField(null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
     title = models.CharField(max_length=20, blank=True)
     content = models.TextField()
--- thewallapi/views.py
+++ thewallapi/views.py
@@ -3,7 +3,10 @@ from rest_framework import viewsets, mixins
 from .models import Post
 from .serializers import PostSerializer
 
-
-class PostViewSet(viewsets.GenericViewSet, mixins.ListModelMixin):
+'''
+GET Get all posts order by its creation time (Desc)
+POST Create a new post
+'''
+class PostViewSet(viewsets.GenericViewSet, mixins.ListModelMixin, mixins.CreateModelMixin):
     queryset = Post.objects.all().order_by('-created_at')
     serializer_class  = PostSerializer
