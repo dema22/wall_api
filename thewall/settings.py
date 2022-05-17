865360bfd5f658188e67bf356cc480ca1abd001a
dema22 <felipedemaria@hotmail.com>
2022-05-17 00:50:39 -0300

    Added django-cors-headers

--- thewall/settings.py
+++ thewall/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'thewallapi.apps.ThewallapiConfig', ## Tell django to recognize our application
+    'corsheaders',
     'rest_framework',
     'django.contrib.admin',
     'django.contrib.auth',
@@ -45,6 +46,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -145,3 +147,5 @@ SIMPLE_JWT = {
     'ALGORITHM': 'HS512',
     'SIGNING_KEY': '123',
 }
+
+CORS_ORIGIN_ALLOW_ALL = True
